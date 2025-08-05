d2f099264dfb52d3e62109cb783d095e80d5178e
jonorl <39122230+jonorl@users.noreply.github.com>
2025-08-05 14:25:41 +0300

    Fix: Pagination going back fixed

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -6,15 +6,15 @@ export const PaginationButtons = () => {
   return (
     <div className="flex justify-center p-4">
       {currentPage > 1 && <button className='mx-1 px-3 py-1 rounded-md bg-gray-200 text-gray-700 hover:bg-gray-300'
-        onClick={() => {setCurrentPage(currentPage - 1); () => postChangePage(currentPage-1); window.location.reload()}}>&lt;</button>}
+        onClick={() => { const newPage = currentPage - 1; setCurrentPage(newPage) }}>&lt;</button>}
       <button
         key={currentPage}
         className={`mx-1 px-3 py-1 rounded-md bg-blue-500 text-white`}
       >
         {currentPage}
       </button>
-      <button className='mx-1 px-3 py-1 rounded-md bg-gray-200 text-gray-700 hover:bg-gray-300' 
-      onClick={() => {const newPage = currentPage + 1; setCurrentPage(parseInt(newPage)); console.log("newPage", newPage)}}>&gt;</button>
+      <button className='mx-1 px-3 py-1 rounded-md bg-gray-200 text-gray-700 hover:bg-gray-300'
+        onClick={() => { const newPage = currentPage + 1; setCurrentPage(parseInt(newPage)) }}>&gt;</button>
     </div>
   );
 };
