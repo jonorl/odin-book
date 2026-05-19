d0c8a90f7a1a8d4ab64e60f77b2518d59db426d4
Jonathan Orlowski <39122230+jonorl@users.noreply.github.com>
2026-05-19 14:01:40 +0200

    Fix: Wrong path

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  base: '/odin-book/',
+  base: './',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
